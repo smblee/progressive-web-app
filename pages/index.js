04507855fe3d6ed9e4df1e4a076074a67bb02ebb
Bryan Lee <smblee14@gmail.com>
2021-01-20 10:14:04 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import styles from '../styles/Home.module.css'
+import Router from 'next/router';
 
 export default function Home() {
+  console.log(Router);
   return (
     <div className={styles.container}>
       <main className={styles.main}>
